8fe0815cd5f1838f64b21f3a72ab86754ccec80c
troynj <troynjohnson6@gmail.com>
2023-03-01 21:39:44 -0800

    pushing to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,48 +15,54 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 // Export a function we will use to POST to the database.
-export const postDb = async (name, home, cell, email)  => {
-  console.log('Post to the database');
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
-
-  // Get confirmation of the request.
+// Exporting the function as a constant using ES6 syntax
+export const putDb = async (content) => {
+  // Logging a message to the console to indicate that the function has been called
+  console.log('PUT to the database', content);
+  
+  // Opening the 'jate' database with version number 1 using the openDB function and waiting for it to complete
+  const connectDb = await openDB('jate', 1);
+  
+  // Creating a transaction on the 'jate' object store in read-write mode
+  const tx = connectDb.transaction('jate', 'readwrite');
+  
+  // Accessing the object store named 'jate' using the transaction object
+  const store = tx.objectStore('jate');
+  
+  // Creating a request to update the record in the store with the given id and content
+  const request = store.put({ id: 1, text: content });
+  
+  // Waiting for the request to complete and storing the result in a variable
   const result = await request;
+  
+  // Logging a message to the console indicating that the data has been saved
   console.log('🚀 - data saved to the database', result);
 };
 
+
 // TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
+// export const getOneDb = async () => console.error('getOneDb not implemented');
 // Export a function we will use to GET to the database.
-export const getDb = async () => {
+// Export a function we will use to GET from the database.
+export const getOneDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const connectDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+   // Create a new transaction and specify the database and data privileges.
+  const tx = connectDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
+   // Use the .get() method to get a piece of data from the database based on the id.
+  const request = store.get(1);
+console.log("line 61", request)
   // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+  const { text } = await request;
+  console.log('result.value', text);
+  return text;
 };
 
 //Start Database
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,5 +1,5 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, postDb } from './database';
+import { getOneDb, putDb } from './database';
 import { header } from './header';
 
 export default class {
@@ -24,9 +24,10 @@ export default class {
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+    getOneDb().then((text) => {
+      console.info('Loaded data from IndexedDB, injecting into editor', text);
+      this.editor.setValue(text || localData || header);
+      console.log("After: ", this.editor.value)
     });
 
     this.editor.on('change', () => {
